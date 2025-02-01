0b7e7ad2bcfb78534f1fad36cfaf00212f65a293
Mohammad Islam <hi@mislam.dev>
2025-02-01 02:01:43 -0500

    Display placeholder until work done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+#!/usr/bin/env node
 
 process.on('SIGINT', () => {
 	console.log('\nGracefully shutting down')
@@ -7,9 +7,9 @@ process.on('SIGINT', () => {
 
 function main() {
 	try {
-		console.log('🎉 Party time! Your app is ready to rock!\n')
-		console.log('👉 Edit \x1b[38;5;208msrc/index.ts\x1b[0m and watch the magic happen here!')
-		console.log('   Lets build something amazing!')
+		console.log(
+			'We are still working on the generator!\nUntil then, follow the Quick Start in the README.'
+		)
 	} catch (error) {
 		console.error('Failed to start:', error)
 		process.exit(1)
